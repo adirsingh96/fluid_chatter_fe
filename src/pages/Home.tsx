5352ee98653bdccbe5a8480b6d334ce4ef690887
adi <adirsingh96@gmail.com>
2024-01-13 19:35:34 +0530

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,29 @@
+import { Box, useMediaQuery, useTheme } from "@mui/material";
+import React from "react";
+
+
+const Home = () => {
+  const theme = useTheme();
+  const isBelowMd = useMediaQuery(theme.breakpoints.down("md"));
+  return (
+    <Box width={"100%"} height={"100%"}>
+      <Box
+        sx={{
+          display: "flex",
+          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          mx: "auto",
+          mt: 3,
+        }}
+      >
+       
+
+
+      </Box>
+      
+    </Box>
+  );
+};
+
+export default Home;
