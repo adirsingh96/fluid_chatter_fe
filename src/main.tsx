c40da3eaed174e93f117212d1b1496527330c179
adi <adirsingh96@gmail.com>
2024-01-14 00:04:49 +0530

    baseURL changed

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,10 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.tsx";
 import { Toaster } from "react-hot-toast";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:5000/api/v1";
+
+
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || "http://localhost:5000/api/v1";
+
 axios.defaults.withCredentials = true;
 const theme = createTheme({
   typography: {
