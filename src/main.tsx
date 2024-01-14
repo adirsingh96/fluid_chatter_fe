e070a22aefde9ceaebad482b65636f2ea5bf731c
adi <adirsingh96@gmail.com>
2024-01-14 12:25:08 +0530

    port changed to 80

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import axios from "axios";
 
 
 
+
 axios.defaults.baseURL = "http://localhost:5000/api/v1";
 
 axios.defaults.withCredentials = true;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
+/*import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+})*/
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+
+
+// Determine the port based on environment variable or default to 3000
+const PORT = 80;
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port:PORT// Use the determined port
+  },
+});
